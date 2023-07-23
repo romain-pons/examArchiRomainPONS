d86359f9c26876925f60ff38af3c3231bb8de1b1
romain-pons <75258269+romain-pons@users.noreply.github.com>
2023-07-24 00:06:42 +0200

    INITIAL COMMIT

--- src/main/java/com/example/examarchiromainpons/entity/CourseEntity.java
+++ src/main/java/com/example/examarchiromainpons/entity/CourseEntity.java
@@ -1,4 +1,32 @@
 package com.example.examarchiromainpons.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "course")
 public class CourseEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_client", referencedColumnName = "id")
+    private UtilisateurEntity client;
+
+    @ManyToOne
+    @JoinColumn(name = "id_chauffeur", referencedColumnName = "id")
+    private UtilisateurEntity chauffeur;
+
+    @Column(name = "date_course")
+    private LocalDate dateCourse;
+
+    @Column(name = "temps_trajet")
+    private int tempsTrajet;
+
+    @Column(name = "distance")
+    private float distance;
 }
