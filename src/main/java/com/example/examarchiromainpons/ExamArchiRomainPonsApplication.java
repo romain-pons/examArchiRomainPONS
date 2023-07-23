d37d2d8f0ecc880ebf6edcf83a84e1df2a5d8c5f
romain-pons <75258269+romain-pons@users.noreply.github.com>
2023-07-23 23:10:20 +0200

    INITIAL COMMIT

---
+++ src/main/java/com/example/examarchiromainpons/ExamArchiRomainPonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.examarchiromainpons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamArchiRomainPonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamArchiRomainPonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examarchiromainpons/ExamArchiRomainPonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examarchiromainpons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamArchiRomainPonsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
